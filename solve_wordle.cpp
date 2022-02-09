1d7048c215ad748e3cfb68e00289272bd53c1cb1
Daniel Robson <danr.236@gmail.com>
2022-02-08 21:52:01 -0500

    wordle list added

--- solve_wordle.cpp
+++ solve_wordle.cpp
@@ -2,7 +2,13 @@
 #include <solver/A2DD.h>
 
 int main(int argc, char **argv) {
+
   std::cout << "hi23" << std::endl;
+
   const auto a = A2DD(0, 1);
+  if (a.getSum() < 10) {
+    std::cout << "OH NO!" << std::endl;
+  }
+
   std::cout << a.getSum() << std::endl;
 }
