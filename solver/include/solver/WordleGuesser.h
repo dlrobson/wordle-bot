797c87c223ade42cce0af679a217e147504437d3
Daniel Robson <danr.236@gmail.com>
2022-02-13 15:30:06 -0500

    formatted cmake files, found longer acceptable word list, added class framework

--- solver/include/solver/WordleGuesser.h
+++ solver/include/solver/WordleGuesser.h
@@ -1,10 +1,47 @@
 #pragma once
 
-class WordleGuesser {
-  int gx;
-  int gy;
+#include <string>
+#include <vector>
 
-public:
-  WordleGuesser(int x, int y);
-  int getSum() const;
+static constexpr uint8_t WORD_LENGTH = 5;
+static constexpr uint8_t NUM_LETTERS = 26;
+
+class WordleGuesser {
+   public:
+    using Word = std::string;
+    using Letters = std::string;
+
+    WordleGuesser(std::string word_list_path);
+
+    std::vector<Word> solve(Word wordle_word);
+
+    // const std::vector<Word>& word_list() const;
+
+   private:
+    struct Statistics {
+        std::vector<uint16_t> letter_count{NUM_LETTERS, 0};
+
+        std::vector<float> letter_freq{NUM_LETTERS};
+
+        std::vector<std::vector<uint16_t>> location_letter_count{
+            WORD_LENGTH, std::vector<uint16_t>(NUM_LETTERS, 0)};
+
+        std::vector<std::vector<float>> location_letter_freq{
+            WORD_LENGTH, std::vector<float>(NUM_LETTERS)};
+    };
+    std::vector<Word> word_list_;
+
+    std::vector<Word> word_guesses_;
+
+    Letters invalid_letters_;
+
+    Letters found_letters_;
+
+    std::vector<Letters> valid_found_letter_locations;
+
+    Statistics list_stats_;
+
+    Word next_word();
+
+    void update_list();
 };
--- solver/src/WordleGuesser.cpp
+++ solver/src/WordleGuesser.cpp
@@ -1,7 +1,49 @@
 #include <solver/WordleGuesser.h>
-WordleGuesser::WordleGuesser(int x, int y) {
-  gx = x;
-  gy = y;
+
+#include <fstream>
+
+static constexpr auto A_OFFSET = 97;
+
+WordleGuesser::WordleGuesser(std::string word_list_path) {
+    std::ifstream file(word_list_path);
+    std::string wordle_word;
+    while (std::getline(file, wordle_word)) {
+        word_list_.emplace_back(wordle_word);
+    }
 }
 
-int WordleGuesser::getSum() const { return gx + gy; }
+std::vector<WordleGuesser::Word> WordleGuesser::solve(
+    WordleGuesser::Word wordle_word) {
+    return word_guesses_;
+}
+
+WordleGuesser::Word WordleGuesser::next_word() {
+    // Generate the count statistics
+    for (const auto& word : word_list_) {
+        for (auto letter_pos_i = 0; letter_pos_i < WORD_LENGTH;
+             letter_pos_i++) {
+            const auto& letter = word[letter_pos_i];
+            const auto letter_index = letter - A_OFFSET;
+            list_stats_.letter_count[letter_index]++;
+            list_stats_.location_letter_count[letter_pos_i][letter_index]++;
+        }
+    }
+
+    // Generate the frequency statistics
+    for (auto letter_i = 0; letter_i < NUM_LETTERS; letter_i++) {
+        const auto& letter_count = list_stats_.letter_count[letter_i];
+        list_stats_.letter_freq[letter_i] =
+            letter_count / static_cast<float>(word_list_.size() * WORD_LENGTH);
+    }
+    // Generate the positional frequency statistics
+    for (auto letter_i = 0; letter_i < NUM_LETTERS; letter_i++) {
+        for (auto letter_pos_i = 0; letter_pos_i < WORD_LENGTH;
+             letter_pos_i++) {
+            const auto& letter_count =
+                list_stats_.location_letter_count[letter_pos_i][letter_i];
+            list_stats_.letter_freq[letter_i] =
+                letter_count / static_cast<float>(word_list_.size());
+        }
+    }
+    return "";
+}
